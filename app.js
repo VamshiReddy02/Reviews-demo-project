e224da2072039a0eaee854ee05d51dfd18baafde
Vamshi Reddy <vamshiproject02@gmail.com>
2023-06-19 18:51:01 +0530

    working on nav toggle part

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+        console.log(links.classList);
+        console.log(links.classList.contains("random"));
+        console.log(links.classList.contains("links"));
+        if (links.classList.contains("show-links")) {
+            links.classList.remove("show-links");
+            } else {
+                links.classList.add("show-links");
+                }
+        links.classList.toggle("show-links");
+});
+
+
 // local reviews data
 const reviews = [
     {
